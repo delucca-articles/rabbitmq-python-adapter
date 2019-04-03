c61cbf5b9897c64917fed9bbddc33a1352e92607
odelucca <delucca@pm.me>
2019-04-03 17:39:55 -0300

    fix(end-to-end): it wasntn really testing

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -2,6 +2,7 @@ import pytest
 import rabbitmq_adapter
 import sys
 import os
+from time import sleep
 
 sys.path.append(os.environ['CONFIG'])
 from config import *
@@ -21,16 +22,24 @@ def setup_listener(
 
     return channel
 
-@pytest.mark.only
+def wait_for_result(
+    anchor,
+    tries=0,
+    retry_after=.6
+):
+    if len(anchor) > 0 or tries > 5: assert len(anchor) > 0
+    else:
+        sleep(retry_after)
+        return wait_for_result(anchor, tries + 1)
+
+@pytest.mark.end_to_end
 def test_rabbitmq_factory():
     # Should be able to create a RabbitMQ connection
-    called = False
+    calls = []
     def mocked_handler(ch, method, props, body):
-        called = True
+        calls.append(1)
         ch.basic_ack(delivery_tag=method.delivery_tag)
         ch.close()
-
-        assert called
 
     rabbitmq_channel = rabbitmq_adapter.channel.create(config.rabbitmq.host)
     rabbitmq_channel = setup_listener(rabbitmq_channel, mocked_handler)
@@ -41,6 +50,7 @@ def test_rabbitmq_factory():
     )
 
     rabbitmq_channel.start_consuming()
+    wait_for_result(calls)
 
 def test_rabbitmq_listen_to_queue():
     # Should be able to listen to a RabbitMQ queue
